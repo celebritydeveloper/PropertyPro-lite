2af925fd9a1bc4de42015ee96a644aa54d38a810
Saviour Essien <eazywebcontact@gmail.com>
2019-06-19 03:07:24 +0100

    Add mobile 320px to 480px query

--- UI/js/app.js
+++ UI/js/app.js
@@ -70,3 +70,22 @@ function fixStepIndicator(n) {
   //... and adds the "active" class to the current step:
   x[n].className += " active";
 }
+
+
+// Open and Close Mobile Nav Bar
+
+const bar = document.querySelector('.nav__mobile-bar');
+const menu = document.querySelector('.nav__menu');
+const close = document.querySelector('.nav__mobile-close');
+
+function openNav () {
+    bar.addEventListener('click', function () {
+        menu.classList.add('active-menu');
+    });
+}
+
+function closeNav () {
+    close.addEventListener('click', function () {
+        menu.classList.remove('active-menu');
+    });
+}
